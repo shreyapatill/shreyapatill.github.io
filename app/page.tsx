d50e5f099e1fd9c8b80a6a7d20baa0cd710ce534
Shreya Patil <shreyap2917@gmail.com>
2025-02-16 20:29:07 -0500

    tailwind customization

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <div>
+    <main className = "relative bg-black-100 flex justify-center items-center flex-col overflow-hidden max-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
         <h1>Hello, Portfolio!</h1>
       </div>
     </main>
