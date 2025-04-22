9e395af14896d8c27d182eb27ed4368941c87010
Shreya Patil <shreyap2917@gmail.com>
2025-04-22 12:08:25 -0400

    changed name

--- lib/imageUtils.ts
+++ lib/imageUtils.ts
@@ -1,5 +1,5 @@
 export function getImageSrc(src: string): string {
   return process.env.NODE_ENV === "production"
-    ? `/personal-website${src}`
+    ? `/shreyapatill.github.io${src}`
     : src;
 }
